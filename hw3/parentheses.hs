f523ee8f50f552aa30f01c877130deb3c97ff1dd
malinovsky239 <malinovsky239@yandex.ru>
2015-05-10 02:12:19 +0300

    task about parentheses updated
    three types of parentheses instead of one

--- hw3/parentheses.hs
+++ hw3/parentheses.hs
@@ -1,14 +1,28 @@
+import Test.HUnit
+
 isCorrect :: String -> Bool
-isCorrect s = isCorrect' s 0
+isCorrect s = isCorrect' s []
   where
-    isCorrect' "" 0 = True
-    isCorrect' "" balance = False
-    isCorrect' ('(' : s) balance = isCorrect' s (balance + 1)
-    isCorrect' (')' : s) balance = if balance == 0 
-                                     then False 
-                                     else isCorrect' s (balance - 1)
-    isCorrect' (first : s) balance = isCorrect' s balance
+    isCorrect' "" [] = True
+    isCorrect' "" _ = False
+    isCorrect' (f:lst) [] = isCorrect' lst [f]
+    isCorrect' (f:lst) (top:stack) | f == rotate top = isCorrect' lst stack
+                                   | otherwise = isCorrect' lst (f:top:stack)
+      where
+        rotate '(' = ')'
+        rotate '[' = ']'
+        rotate '{' = '}'
+        rotate x = x                             
 
-
-
+-- testing
+test1 = TestCase(assertEqual "1" True (isCorrect "([{}])"))
+test2 = TestCase(assertEqual "2" True (isCorrect "[({}[])]"))
+test3 = TestCase(assertEqual "3" False (isCorrect "[({}][])"))
+test4 = TestCase(assertEqual "4" False (isCorrect "([[[]])]"))
+tests = TestList [TestLabel "1" test1, 
+                  TestLabel "2" test2,
+                  TestLabel "3" test3,
+                  TestLabel "4" test4]
+                  
+runTesting = runTestTT tests
  
